6505c2079eb6415dbac1a32c4c6bf1b5bd5a89df
Viacheslav <viacheslav.modestov@gmail.com>
2018-06-25 18:58:02 +0300

    Add utils.js with static and test data

---
+++ src/comments-components/utils.js
@@ -0,0 +1,24 @@
+export const likes_img = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAYAAAAeP4ixAAAAAXNSR0IArs4c    6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAATjSURBVGhD7ZlpzB1THIdfa21BLY0iRWqLaGL7IKi09gRtKBISBGnFGvWhiNjSqFaKoJpKEEtSgpZ    ELLEEEW2RiAixBKUldrFEaS31PKfvX6b3ve+dmTszzfvh/pIn954z55w5d+ac/3JuX0899dRTO20E6635Wkkb93+uMx0EN8Jb8A2s7udreBJOhU0hTzvCFbAYfgfHWAnL4B    m4AHaA2rU/vAAx8eAP+Kul7me4BNaHVm0PD8LfkO3jj2mtc+zZsA3UosshbvItzILDYDiEdobT4Vn4F2z7GuwEoRPhO/Dar3AHjIctQG0AvqnT4BGIB/QlHAiVdDs4mD/EJ    RU37aSD4TOw38fgj5wEq/rrHoftIE/7wPNgnxVwDHSly8BBfoHjrCihLcE9Y3+fqD/CN+WSKyPf0q3gOC7ZvaGUxoBvQY63ogttBkvASciV0K3uA8d4B9rtvUH1NNhxRip1    Lze3Fu1RqGKmNc8fgnM624oi2hfs8ANsZUVFHQl1WJ6TwXm9n0oFdA3YQasylOSS8u06tz2tyNOrYOOjU2lo6R5wbhemUo6Wgo01m03LKGAEFF3CU8G56cty9SfY2DiqCWk    ArgfXejhP0Uw/BJ2cn07Xtg+kUo5+Axtvkkr1SoujX4rJ6711nuHxA82t5rtVZ4LX70+lHH0ONh6ZSvXpaoiJvgxHweYQ2gU09zo+2xhQtv4YwyWv6SRzZYxk4yNSqR5pOl    1GOtjzrOig0fAeOAdjrqzuBuunQa5jvAVsfFUqVZdP1fXvmFOsKCBXgwGqfYyxXJIvQcRrYvxlnUHmhjBA4XgWpVJ1nQuO503L6AywX9YgaIjMWZZn6uRNGOBbfIKG2Q7ga    66qheDNTkml4jIsieTNRG4CZJO2reEcMMK2jZGIMeJauhe8ODeVqkmL5N7I5i9F5TzMJDvtBx+8e8n5fgFG3v9rL/gHfJW7WtGldHTewD3SjYq6AMP9F8F73WZFVqakXjCH    7lajwDHeTaVmtTv48N0WayWAJv8/gROZbEUX0vrY/4NUal7PgfebmEoZnQVe8CDAE5Sy8pXrvV2ifm9a14LzNYIfoIg43bTm0WUVzu2QVGpWHiN5r5tSqUXDILz9V+BaLKM    5YN+bU6lZxRu5IZXaSOvjhrWRzqiMfzHUsd+n0PTyij1yUioNIsPvt8GG38PhUETm6nE8pLduSuZPsR9zU2u9aWSQHnEafhTRRWAfDxCaeisRUJrTFJIJ1zywk9wJ7qNO8r    pe1/aXWlGzXL5GD76NPawoIy1ERKMuubwBTgDbmriZe9QlLWkkZm3NbhGNhXjSTtAMrpMeBtsaTpQ6aBtEhvjxI56CSsvWYNADOAeT+TDYYYIHDT+C7arkO0bBM8GQxLG0V    kX+zigkMz/jHAf2JKbVAfq0fPVxyu7noVBG7s/zIfIRUw0P2tsmVlW0G7wCMdHrwJv494J5etS/3v/dJGlbyJN+y3xeh2w/0QKa9zcm177nT1oQb/gGmPT43f3kfysmTREt    eM7c7lzYk3eXn9lfNkv8BIwB10XslnQARPYmCyCbXBldxxOO5Mml5uHbRxD9xIfyBBjV1r6Mishs7TG4OJUGahxo/11ucdgQaAX9U8h0tra/35rUdIjJu/TugmMhz8kOOWm    JPLzbL5V66qmnnoqrr+8/sFhleR8OUN4AAAAASUVORK5CYII="
+export const model = {posts:[
+     {id: 1, post:{avatar:
+ "http://www.avatarsdb.com/avatars/normal_people_scare_me.jpg",
+         msg:"Here comes the Message.",
+         isTop:true, likes:1, sub_posts:[
+             {id: 1.1, post:{
+ avatar:"http://www.avatarsdb.com/avatars/normal_people_scare_me.jpg",
+                 msg:"Very good Message.",
+         isTop:false, likes:0}},
+             {id:1.2, post:{
+ avatar:"http://www.avatarsdb.com/avatars/normal_people_scare_me.jpg",
+                 msg:"I like the Message!",
+         isTop:false, likes:0}},
+         ]}
+     }, // top post with sub posts
+     {id:2, post:{
+     avatar:"http://www.avatarsdb.com/avatars/normal_people_scare_me.jpg",
+         msg:"This Message is going to be even better.",
+         isTop:true, likes:3}
+ }, // top message without sub posts
+ 
+ ]};
+
